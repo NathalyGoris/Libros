6bfab59ffec19aa085e439ad7505eb74d941e535
Nathaly Goris Espino <nathalygorisespino@outlook.com>
2023-06-05 16:44:50 -0400

    Ejemplo

---
+++ Libros/DAL/Context.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameWorkCore;
+using Libros.Models;
+
+public class Context : DbContext
+{
+    public Context(DbContextOptions<Libros> Options)
+        :base(Options){}
+
+    public DbSet<Libros> Libros {get; set;}
+  
+}
---
+++ Libros/Models/Libro.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+public class Libro
+    {
+        [Key]
+        public int LibroId { get; set; }
+
+        [Required (ErrorMessage = "El nombre del libro es obligatorio")]
+        public string? Nombre { get; set; }
+
+        [Required (ErrorMessage = "El Precio del libro es obligatorio")]
+        public string? Precio { get; set; }
+    }
